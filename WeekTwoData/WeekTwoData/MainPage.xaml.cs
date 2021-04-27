ae585b5b6ffedaeec32cf2f13fac8cd388828be9
DominicB-yte <53456554+DominicB-yte@users.noreply.github.com>
2021-04-27 19:21:45 +1200

    Initial commit

---
+++ WeekTwoData/WeekTwoData/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeekTwoData
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            label1.BindingContext = slider1;
+            label1.SetBinding(Label.RotationProperty, "Value");
+        }
+    }
+}
